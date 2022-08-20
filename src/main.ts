b7eeab8f2b496cc4a292cf2fb60b6a6a6a2bc0aa
Fernando Orozco <forozcomercado@gmail.com>
2022-08-20 13:37:24 -0500

    ADD ValidationPipe as a global pipe to strip properties without validation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // strip validated object of any properties that do not use any validation decorator
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   await app.listen(3000);
 }
 bootstrap();
