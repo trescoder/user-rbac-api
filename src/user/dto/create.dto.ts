9089aa13702ca8ec32b4cde676ac86418a8aace7
Fernando Orozco <forozcomercado@gmail.com>
2022-08-20 13:37:49 -0500

    ADD DTO to create a new account

---
+++ src/user/dto/create.dto.ts
@@ -0,0 +1,16 @@
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateAccountDTO {
+  @IsString()
+  @IsNotEmpty()
+  username: string;
+
+  @IsString()
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  password: string;
+}
