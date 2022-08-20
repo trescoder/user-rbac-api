01f9b4e3f8d0392361497c9023e37fb19afd05b3
Fernando Orozco <forozcomercado@gmail.com>
2022-08-20 15:17:10 -0500

    UPDATE set up to use the different strategies

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
 import { AuthService } from './auth.service';
+import { LocalStrategy } from './local.strategy';
+import { JwtStrategy } from './jwt.strategy';
+import { AuthController } from './auth.controller';
 
 @Module({
-  providers: [AuthService],
+  imports: [PassportModule],
+  providers: [AuthService, LocalStrategy, JwtStrategy],
+  controllers: [AuthController],
 })
 export class AuthModule {}
