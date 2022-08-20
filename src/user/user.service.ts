de5ef411da5cc5871254f9ab70192a5e88ad8e09
Fernando Orozco <forozcomercado@gmail.com>
2022-08-20 14:18:41 -0500

    ADD method to check username and email availability to the user service

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UserEntity } from 'src/entities/user.entity';
+import { DataSource, Repository } from 'typeorm';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(
+    @InjectRepository(UserEntity)
+    private userRepository: Repository<UserEntity>,
+    private dataSource: DataSource,
+  ) {}
+
+  async checkUsernameAndEmailAvailability(username, email) {
+    // search for a user with a specific username or email
+    const user = await this.dataSource
+      .getRepository(UserEntity)
+      .createQueryBuilder('user')
+      .where('user.username = :username', { username })
+      .orWhere('user.email = :email', { email });
+    if (user) {
+      return false;
+    }
+    return true;
+  }
+}
