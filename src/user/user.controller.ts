c1929d8b753d8481e730db1f2b73acbc601d77eb
Fernando Orozco <forozcomercado@gmail.com>
2022-08-20 15:13:45 -0500

    ADD route to create account

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,10 +1,15 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Res } from '@nestjs/common';
+import { Response } from 'express';
 import { CreateAccountDTO } from './dto/create-account.dto';
+import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
+  constructor(private userService: UserService) {}
+
   @Post('sign-in')
-  signIn(@Body() body: CreateAccountDTO) {
-    return body;
+  async signIn(@Body() body: CreateAccountDTO, @Res() res: Response) {
+    const { status, ...data } = await this.userService.createAccount(body);
+    return res.status(status).json(data);
   }
 }
