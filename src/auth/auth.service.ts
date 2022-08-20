4cc5f975c8eb72f62df99f84d5ea6f76e130a0cf
Fernando Orozco <forozcomercado@gmail.com>
2022-08-20 17:26:27 -0500

    ADD method to validate user credentials

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { UserService } from 'src/user/user.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private userService: UserService) {}
+
+  async validateUserCredential(email: string) {
+    const user = await this.userService.findUser(email);
+    return user;
+  }
+}
