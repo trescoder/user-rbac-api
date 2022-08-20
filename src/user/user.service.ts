41444461d1694d17d74c52cfaef4e928ad3e7bcf
Fernando Orozco <forozcomercado@gmail.com>
2022-08-20 15:13:04 -0500

    ADD method to create a new account REMOVE method to check username and email availability

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { ResponseInterface } from 'src/common/interfaces/response';
 import { UserEntity } from 'src/entities/user.entity';
 import { DataSource, Repository } from 'typeorm';
+import { CreateAccount } from './create-account.interface';
 
 @Injectable()
 export class UserService {
@@ -11,16 +13,17 @@ export class UserService {
     private dataSource: DataSource,
   ) {}
 
-  async checkUsernameAndEmailAvailability(username, email) {
-    // search for a user with a specific username or email
-    const user = await this.dataSource
-      .getRepository(UserEntity)
-      .createQueryBuilder('user')
-      .where('user.username = :username', { username })
-      .orWhere('user.email = :email', { email });
-    if (user) {
-      return false;
+  async createAccount(accountData: CreateAccount): Promise<ResponseInterface> {
+    try {
+      await this.dataSource
+        .createQueryBuilder()
+        .insert()
+        .into(UserEntity)
+        .values([accountData])
+        .execute();
+      return { status: 200, ok: true, msg: 'User Account Created' };
+    } catch (e) {
+      return { status: 400, ok: false, msg: e.detail };
     }
-    return true;
   }
 }
