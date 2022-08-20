1c0cd0d523f6c2a410002f4fccc40be6c9b0f027
Fernando Orozco <forozcomercado@gmail.com>
2022-08-20 13:43:01 -0500

    ADD post route to create a new account, so far, it only return the incoming data.

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,10 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateAccountDTO } from './dto/create.dto';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  @Post('sign-in')
+  signIn(@Body() body: CreateAccountDTO) {
+    return body;
+  }
+}
