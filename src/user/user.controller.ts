aaac9062a49ba9dfc6d07d068e925318c5fe00ac
Fernando Orozco <forozcomercado@gmail.com>
2022-08-21 09:36:17 -0500

    ADD route to get the user profile

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,5 +1,14 @@
-import { Body, Controller, Post, Res } from '@nestjs/common';
-import { Response } from 'express';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Req,
+  Res,
+  UseGuards,
+} from '@nestjs/common';
+import { Request, Response } from 'express';
+import { JwtGuard } from 'src/auth/jwt-strategy/jwt.guard';
 import { CreateAccountDTO } from './dto/create-account.dto';
 import { UserService } from './user.service';
 
@@ -11,5 +20,11 @@ export class UserController {
   async signIn(@Body() body: CreateAccountDTO, @Res() res: Response) {
     const { status, ...data } = await this.userService.createAccount(body);
     return res.status(status).json(data);
+  }
+
+  @Get('profile')
+  @UseGuards(JwtGuard)
+  async profile(@Req() req: Request) {
+    return req.user;
   }
 }
