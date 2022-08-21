17951a1cb254d9f27ea623b2633c7d3302bac0c9
Fernando Orozco <forozcomercado@gmail.com>
2022-08-21 09:27:39 -0500

    ADD method to login that generates an access token with the user's email and password

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { comparePasswords } from 'src/bcrypt-manager';
 import { UserService } from 'src/user/user.service';
 
 @Injectable()
 export class AuthService {
-  constructor(private userService: UserService) {}
+  constructor(
+    private userService: UserService,
+    private jwtService: JwtService,
+  ) {}
 
   async validateUserCredential(email: string, password: string) {
     const { user } = await this.userService.findUser(email);
@@ -12,5 +16,11 @@ export class AuthService {
       return null;
     }
     return user;
+  }
+
+  // this method sign some user information that will be later use to authenticate this user
+  async login(user: { password: string; email: string }) {
+    const token = this.jwtService.sign(user);
+    return { token };
   }
 }
