03c37a8c11d0cad25d8e34f0092387568c7bc257
Fernando Orozco <forozcomercado@gmail.com>
2022-08-21 09:29:09 -0500

    ADD jwt strategy set up done

--- src/auth/jwt-strategy/jwt.strategy.ts
+++ src/auth/jwt-strategy/jwt.strategy.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
 
 @Injectable()
-export class JwtStrategy {}
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(), // instructs jwt from where it will extract the token
+      ignoreExpiration: false,
+      secretOrKey: 'supersecret',
+    });
+  }
+
+  // this method is calling when using the (in this case) JwtGuard
+  async validate(payload) {
+    const { email, password } = payload;
+    // after the token have been decoded we will have a user in payload, we only want the email, and password
+    return { email, password };
+  }
+}
