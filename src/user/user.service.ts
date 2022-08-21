0fc31c45ab14a0b15ef6a697e79d338e8944c1be
Fernando Orozco <forozcomercado@gmail.com>
2022-08-21 10:56:41 -0500

    ADD method to add a new post ADD method to get user profile with is posts

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,10 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Post } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { ResponseInterface } from 'src/common/interfaces/response';
 import { UserEntity } from 'src/entities/user.entity';
 import { DataSource, Repository } from 'typeorm';
 import { CreateAccount } from './create-account.interface';
 import { hashPassword } from 'src/bcrypt-manager';
+import { PostEntity } from 'src/entities/post.entity';
 @Injectable()
 export class UserService {
   constructor(
@@ -13,6 +14,7 @@ export class UserService {
     private dataSource: DataSource,
   ) {}
 
+  // TODO: Refactor and/or documentation
   async createAccount(accountData: CreateAccount): Promise<ResponseInterface> {
     // encrypt password before store
     accountData.password = hashPassword(accountData.password);
@@ -31,10 +33,55 @@ export class UserService {
     }
   }
 
-  async findUser(email) {
+  // TODO: Refactor and/or documentation
+  async findUser(email: any) {
     try {
-      const user = await this.userRepository.findOneBy({ email });
-      return { user, ok: true };
+      let user;
+      if (typeof email === 'string') {
+        user = await this.userRepository.findOneBy({ email });
+      } else {
+        user = await this.userRepository.findOneBy({ email: email.email });
+      }
+
+      return { user };
+    } catch (error) {
+      return { error, ok: false };
+    }
+  }
+
+  // TODO: Refactor and/or documentation
+  async getUserProfile(email: any): Promise<ResponseInterface> {
+    let userInfo;
+    // this part gets the user
+    try {
+      ({ user: userInfo } = await this.findUser(email));
+    } catch (error) {
+      return { ok: false, status: 500, msg: error.msg };
+    }
+
+    // this part gets the user's posts
+    try {
+      let userPosts = await this.dataSource
+        .getRepository(UserEntity)
+        .createQueryBuilder('user')
+        .leftJoinAndSelect('user.posts', 'post')
+        .getMany();
+      userPosts = userPosts.filter((u) => u.id === userInfo.id);
+      return { ok: true, status: 200, data: userPosts };
+    } catch (error) {
+      return { ok: false, status: 500, msg: error.msg };
+    }
+  }
+
+  // TODO: Refactor and/or documentation
+  async savePost(user: UserEntity, postContent: string) {
+    try {
+      const post = new PostEntity();
+      post.content = postContent;
+      post.owner = user;
+      await this.dataSource.manager.save(post);
+      user.posts.push(post);
+      await this.dataSource.manager.save(user);
     } catch (error) {
       return { error, ok: false };
     }
