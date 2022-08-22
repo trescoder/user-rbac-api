85c73213510b9d3eeeceebc71723e1895b3464a1
Fernando Orozco <forozcomercado@gmail.com>
2022-08-22 14:03:29 -0500

    ADD TypeORM configuration file to use the .env variables

---
+++ src/typeOrmConfigService.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm';
+import { LikeEntity } from './entities/like.entity';
+import { PostEntity } from './entities/post.entity';
+import { UserEntity } from './entities/user.entity';
+
+@Injectable()
+export class TypeOrmConfigService implements TypeOrmOptionsFactory {
+  constructor(private configService: ConfigService) {}
+
+  createTypeOrmOptions(
+    connectionName?: string,
+  ): TypeOrmModuleOptions | Promise<TypeOrmModuleOptions> {
+    return {
+      type: 'postgres',
+      host: this.configService.get('DB_HOST'),
+      port: +this.configService.get('DB_PORT'),
+      username: this.configService.get('DB_USERNAME'),
+      password: this.configService.get('DB_PASSWORD'),
+      database: this.configService.get('DB_NAME'),
+      entities: [UserEntity, PostEntity, LikeEntity],
+      synchronize: true,
+    };
+  }
+}
