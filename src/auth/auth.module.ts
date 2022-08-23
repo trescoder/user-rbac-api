231ea631390e7101db2944ee576827b12af51edd
Fernando Orozco <forozcomercado@gmail.com>
2022-08-23 14:52:39 -0500

    ADD includes Repository Module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -8,6 +8,7 @@ import { UserModule } from 'src/user/user.module';
 import { JwtModule } from '@nestjs/jwt';
 import { APP_GUARD } from '@nestjs/core';
 import { JwtGuard } from './jwt-strategy/jwt.guard';
+import { RepositoryModule } from 'src/shared/repositories/repository.module';
 
 @Module({
   imports: [
@@ -17,6 +18,7 @@ import { JwtGuard } from './jwt-strategy/jwt.guard';
       secret: 'supersecret',
       signOptions: { expiresIn: '50m' },
     }),
+    RepositoryModule,
   ],
   providers: [
     AuthService,
