5e3c3df360dab9f6d507c50270d1763b6803b025
Fernando Orozco <forozcomercado@gmail.com>
2022-08-23 14:52:58 -0500

    ADD user repository service

--- src/shared/repositories/user-repository/user-repository.service.ts
+++ src/shared/repositories/user-repository/user-repository.service.ts
@@ -1,4 +1,49 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { PostEntity } from 'src/entities/post.entity';
+import { UserEntity } from 'src/entities/user.entity';
+import { DataSource, In, Repository } from 'typeorm';
 
 @Injectable()
-export class UserRepositoryService {}
+export class UserRepositoryService {
+  constructor(
+    @InjectRepository(UserEntity)
+    private userRepository: Repository<UserEntity>,
+    @InjectRepository(PostEntity)
+    private postRepository: Repository<PostEntity>,
+    private dataSource: DataSource,
+  ) {}
+
+  async findUserByEmail(email: string) {
+    try {
+      const user = await this.userRepository.findOneBy({ email });
+      return user;
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+
+  // TODO: Don't return entities but DTOs
+  async getUserProfile(userData: any) {
+    try {
+      // retrieve user and posts
+      const userWithPosts = await this.userRepository.findOne({
+        relations: { posts: true },
+        where: { id: userData.id },
+      });
+      // extract the posts ids
+      const postIds = userWithPosts.posts.map((p) => p.id);
+
+      // retrieve all posts that belong to this user with its likes/dislikes
+      const postWithLikes = await this.postRepository.find({
+        relations: { likes: true },
+        where: { id: In(postIds) },
+      });
+
+      userWithPosts.posts = postWithLikes;
+      return userWithPosts;
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+}
