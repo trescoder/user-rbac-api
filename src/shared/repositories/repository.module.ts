d9abe06f45a8c58682fddf6ea098b47a5c1a8aab
Fernando Orozco <forozcomercado@gmail.com>
2022-08-23 14:52:21 -0500

    ADD TypeOrmModule configuration

--- src/shared/repositories/repository.module.ts
+++ src/shared/repositories/repository.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { LikeEntity } from 'src/entities/like.entity';
+import { PostEntity } from 'src/entities/post.entity';
+import { UserEntity } from 'src/entities/user.entity';
 import { LikeRepositoryService } from './like-repository/like-repository.service';
 import { PostRepositoryService } from './post-repository/post-repository.service';
 import { UserRepositoryService } from './user-repository/user-repository.service';
 
 @Module({
   imports: [
+    RepositoryModule,
+    TypeOrmModule.forFeature([UserEntity, PostEntity, LikeEntity]),
+  ],
+  providers: [
     LikeRepositoryService,
     UserRepositoryService,
     PostRepositoryService,
