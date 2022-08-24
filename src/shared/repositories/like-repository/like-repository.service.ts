cd69ca219fc7dbb49a7764f027de83fc4bc06a4d
Fernando Orozco <forozcomercado@gmail.com>
2022-08-24 10:35:23 -0500

    UPDATE method to update the likes of a post, now depending on different conditions, the like/dislike is added, changed or removed.

--- src/shared/repositories/like-repository/like-repository.service.ts
+++ src/shared/repositories/like-repository/like-repository.service.ts
@@ -16,22 +16,53 @@ export class LikeRepositoryService {
     private likeRepository: Repository<LikeEntity>,
   ) {}
 
-  async savePost({ postId, userId, like }) {
+  async updateLikes({ postId, userId, like }) {
     try {
       const post = await this.postRepository.findOne({
         relations: { likes: true },
         where: { id: postId },
       });
 
-      const likeEntity = new LikeEntity();
-      likeEntity.isLike = like;
-      likeEntity.post = post;
-      likeEntity.userId = userId;
+      // check if the suer already liked/disliked the post
+      if (post.likes.some((l) => l.userId === userId)) {
+        const userLike = post.likes.find((l) => l.userId === userId);
+        // check if the user is removing his like/dislike
 
-      await this.likeRepository.save(likeEntity);
-      post.likes.push(likeEntity);
-      await this.postRepository.save(post);
+        if (userLike.isLike === like) {
+          await this.removeLike(userLike.id);
+          return { msg: 'like/dislike removed' };
+        } else {
+          // here switches from like to dislike or vice versa
+          userLike.isLike = like;
+          await this.likeRepository.save(userLike);
+          return { msg: 'like/dislike switch' };
+        }
+      } else {
+        // first time liking/disliking
+        const likeEntity = new LikeEntity();
+        likeEntity.isLike = like;
+        likeEntity.post = post;
+        likeEntity.userId = userId;
+
+        await this.likeRepository.save(likeEntity);
+        post.likes.push(likeEntity);
+        await this.postRepository.save(post);
+        return { msg: 'new like/dislike added' };
+      }
     } catch (error) {
+      console.log(error);
+
+      throw new Error(error);
+    }
+  }
+
+  async removeLike(id: number) {
+    try {
+      const like = await this.likeRepository.findOneBy({ id });
+      await this.likeRepository.delete(like);
+    } catch (error) {
+      console.log(error);
+
       throw new Error(error);
     }
   }
