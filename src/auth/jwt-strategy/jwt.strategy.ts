e83a39828cd205b55faa90e6429500a689c67fa3
Fernando Orozco <forozcomercado@gmail.com>
2022-08-25 15:45:57 -0500

    ADD now when you create a token, and removed the account that generated that token, the server throws a 404 http exception instead of a 500 server error

--- src/auth/jwt-strategy/jwt.strategy.ts
+++ src/auth/jwt-strategy/jwt.strategy.ts
@@ -1,10 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
+import { UserRepositoryService } from 'src/shared/repositories/user-repository/user-repository.service';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor() {
+  constructor(private userService: UserRepositoryService) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(), // instructs jwt from where it will extract the token
       ignoreExpiration: false,
@@ -15,6 +16,13 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   // this method is calling when using the (in this case) JwtGuard
   async validate(payload) {
     const { email, id } = payload;
+
+    const user = await this.userService.findUserBy({ id });
+    if (!user) {
+      throw new NotFoundException({
+        message: 'Seems like this account does not exists',
+      });
+    }
     // after the token have been decoded we will have the user email available in the payload
     return { email, id };
   }
