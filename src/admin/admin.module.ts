34af315cc36d32bfcfb92a1bf678a5e3a2057f7a
Fernando Orozco <forozcomercado@gmail.com>
2022-08-26 11:03:27 -0500

    ADD Admin Module File

--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { RepositoryModule } from 'src/shared/repositories/repository.module';
 import { AdminController } from './admin.controller';
 import { AdminService } from './admin.service';
 
 @Module({
+  imports: [RepositoryModule],
   controllers: [AdminController],
-  providers: [AdminService]
+  providers: [AdminService],
 })
 export class AdminModule {}
