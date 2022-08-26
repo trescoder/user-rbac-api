933daf1f3ee0d0c0f251efe50597ba93c2ef994e
Fernando Orozco <forozcomercado@gmail.com>
2022-08-26 10:04:00 -0500

    ADD admin module ADD admin controller ADD admin service

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,7 @@ import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmConfigService } from './typeOrmConfigService';
+import { AdminModule } from './admin/admin.module';
 
 @Module({
   imports: [
@@ -18,6 +19,7 @@ import { TypeOrmConfigService } from './typeOrmConfigService';
     }),
     AuthModule,
     UserModule,
+    AdminModule,
   ],
   controllers: [AppController],
   providers: [AppService],
