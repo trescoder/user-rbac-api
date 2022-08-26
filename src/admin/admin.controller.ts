5f8390ccd36deae561e2bc5c309dcad02f91f43b
Fernando Orozco <forozcomercado@gmail.com>
2022-08-26 11:03:17 -0500

    ADD route to get any user profile

--- src/admin/admin.controller.ts
+++ src/admin/admin.controller.ts
@@ -1,4 +1,25 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  ParseIntPipe,
+  Req,
+  Res,
+} from '@nestjs/common';
+import { Response } from 'express';
+import { AllowedRoles } from 'src/auth/roles.decorator';
+import { Roles } from 'src/roles';
+import { AdminService } from './admin.service';
 
+@AllowedRoles(Roles['semi-admin'], Roles.admin)
 @Controller('admin')
-export class AdminController {}
+export class AdminController {
+  constructor(private adminService: AdminService) {}
+
+  @Get('get-user/:id')
+  async getUsersData(@Param('id', ParseIntPipe) id, @Res() res: Response) {
+    const { status, data } = await this.adminService.getUserData(id);
+    return res.status(status).json(data);
+  }
+}
