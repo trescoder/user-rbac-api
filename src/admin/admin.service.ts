fff4c93c89d697e8de1c4110cffeb1fd2878d5cb
Fernando Orozco <forozcomercado@gmail.com>
2022-08-26 11:01:13 -0500

    ADD admin servicenow can retrieve the profile of any user

--- src/admin/admin.service.ts
+++ src/admin/admin.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { ResponseInterface } from 'src/shared/interfaces/response';
+import { UserRepositoryService } from 'src/shared/repositories/user-repository/user-repository.service';
 
 @Injectable()
-export class AdminService {}
+export class AdminService {
+  constructor(private userRepoService: UserRepositoryService) {}
+
+  async getUserData(userId: number): Promise<ResponseInterface> {
+    try {
+      if (await this.userRepoService.checkUserIdExistence(userId)) {
+        const user = await this.userRepoService.getUserProfile(userId);
+        return { data: user, ok: true, status: 200 };
+      }
+      return { data: {}, ok: true, status: 404 };
+    } catch (error) {
+      console.log(error);
+
+      return { ok: false, status: 500, msg: error.detail };
+    }
+  }
+}
