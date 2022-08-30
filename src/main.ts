a0d6291fa7ce911d35001dc58cad164f1697e4bb
Fernando Orozco <forozcomercado@gmail.com>
2022-08-30 15:28:27 -0500

    ADD Cors

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // strip validated object of any properties that do not use any validation decorator
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.enableCors({ origin: 'http://localhost:4200' });
   await app.listen(3000);
 }
 bootstrap();
