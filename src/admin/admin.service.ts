4f81d7880fc47f7cd966c708822085e8ed85daf7
Fernando Orozco <forozcomercado@gmail.com>
2022-08-31 10:15:37 -0500

    ADD method to create an admin user

--- src/admin/admin.service.ts
+++ src/admin/admin.service.ts
@@ -1,10 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { Roles } from 'src/roles';
 import { ResponseInterface } from 'src/shared/interfaces/response';
 import { UserRepositoryService } from 'src/shared/repositories/user-repository/user-repository.service';
+import { UserService } from 'src/user/user.service';
 
 @Injectable()
 export class AdminService {
-  constructor(private userRepoService: UserRepositoryService) {}
+  constructor(
+    private userRepoService: UserRepositoryService,
+    private userService: UserService,
+  ) {}
 
   async getUserData(userId: number): Promise<ResponseInterface> {
     try {
@@ -18,5 +23,14 @@ export class AdminService {
 
       return { ok: false, status: 500, msg: error.detail };
     }
+  }
+
+  async createAdminUser() {
+    return this.userService.createAccount({
+      username: process.env.ADMIN_USERNAME,
+      role: Roles[process.env.ADMIN_ROLE],
+      password: process.env.ADMIN_PASSWORD,
+      email: process.env.ADMIN_EMAIL,
+    });
   }
 }
